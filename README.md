6e9f181c8d77ab080db32aa8e345bfd72000345b
André Henrique DA SILVA <andrehenrique.dasilva@orange.com>
2021-12-29 17:54:16 +0100

    Add AHQSLogger

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
 # AHQSLogger
 
-A description of this package.
+A simple logging system.
+
+## Usage
+
+    import AHQSLogger
+
+Use the following methods for loggging. 
+
+## Logging an information / debug
+
+You can log a simple message:
+
+        AHQSLogger.shared.info("Hello world!")
+        
+        
+Additionally you can pass an object as a parameter:
+
+        let values = ["hello", "foo", "bar"]
+
+        AHQSLogger.shared.info("Hello world!", containing: values)
+        
+        
+## Logging an error
+
+You can either log an Error object or an NSError object.
+
+
+### Error
+
+    enum MyCustomError: LocalizedError {
+        case invalid
+        case unknown
+    
+        var errorDescription: String? {
+            switch self {
+            case .invalid:
+                return "Invalid error."
+            case .unknown:
+                return "Unknown error."
+            }
+        }
+    }
+    
+    let customError = MyError.invalid
+        
+    AHQSLogger.shared.error(customError)
+    
+    
+### NSError
+
+    let error = NSError(domain: "com.ahenqs.logger", code: 1, userInfo: [NSLocalizedDescriptionKey: "Custom NSError here for testing purposes."])
+    AHQSLogger.shared.error(error)
+
+
+## Subsystem and Category
+        
+You can change the subsystem and category for better separation of logs.
+
+    AHQSLogger.shared.setSubsystem("Foo", category: "Bar")
+    
+Or set them separately at run time:
+
+    AHQSLogger.shared.setSubsystem("Other")
+    
+    AHQSLogger.shared.setCategory("MyApp")
--- Sources/AHQSLogger/AHQSLogger.swift
+++ Sources/AHQSLogger/AHQSLogger.swift
@@ -1,6 +1,77 @@
-public struct AHQSLogger {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
+import os.log
 
-    public init() {
+@available(iOS 13.0, *)
+public final class AHQSLogger {
+    
+    /// The singleton instance.
+    public static let shared = AHQSLogger()
+    
+    /// Private initializer. This is a singleton.
+    private init() {}
+    
+    /// A visual indicator for easily spotting logs in the console.
+    public var loggerVisualIndicator: String = "📑"
+    
+    /// A visual indicator for easily spotting the information log in the console.
+    public var loggerInfoVisualIndicator: String = "🗣"
+    
+    /// A visual indicator for easily spotting the error log in the console.
+    public var loggerErrorVisualIndicator: String = "❌"
+        
+    /// Log subsystem.
+    private var subsystem = "Logging"
+    
+    /// Log category.
+    private var category = "Application"
+    
+    /// Adds identification for subsystem and category for better visualization in Console.
+    public func setSubsystem(_ subsystem: String, category: String) {
+        self.subsystem = subsystem
+        self.category = category
+    }
+    
+    /// Adds identification for subsystem for better visualization in Console.
+    public func setSubsystem(_ subsystem: String) {
+        self.subsystem = subsystem
+    }
+    
+    /// Adds category for better visualization in Console.
+    public func setCategory(_ category: String) {
+        self.category = category
+    }
+    
+    /// Logs debug information.
+    /// Additionaly an object can be logged as well.
+    public func info(_ message: String, containing object: Any? = nil) {
+        let log = OSLog(subsystem: self.subsystem, category: self.category)
+        if let object = object as? CVarArg {
+            os_log("%{public}@ %{public}@ %{public}@ : %{public}@", log: log, type: .info, self.loggerVisualIndicator, self.loggerInfoVisualIndicator, message, "\(object)")
+        } else {
+            os_log("%{public}@ %{public}@ %{public}@", log: log, type: .info, self.loggerVisualIndicator, self.loggerInfoVisualIndicator, message)
+        }
+    }
+    
+    /// Logs error with Error object.
+    /// Additionaly an object can be logged as well.
+    public func error(_ error: Error, containing object: Any? = nil) {
+        let log = OSLog(subsystem: self.subsystem, category: self.category)
+        if let object = object as? CVarArg {
+            os_log("%{public}@ %{public}@ %{public}@ : %{public}@", log: log, type: .error, self.loggerVisualIndicator, self.loggerErrorVisualIndicator, error.localizedDescription, "\(object)")
+        } else {
+            os_log("%{public}@ %{public}@ %{public}@", log: log, type: .error, self.loggerVisualIndicator, self.loggerErrorVisualIndicator, error.localizedDescription)
+        }
+    }
+    
+    /// Logs error with NSError object.
+    /// Additionaly an object can be logged as well.
+    public func error(_ error: NSError, containing object: Any? = nil) {
+        let log = OSLog(subsystem: self.subsystem, category: self.category)
+        if let object = object as? CVarArg {
+            os_log("%{public}@ %{public}@ %{public}@ : %{public}@", log: log, type: .error, self.loggerVisualIndicator, self.loggerErrorVisualIndicator, error.localizedDescription, "\(object)")
+        } else {
+            os_log("%{public}@ %{public}@ %{public}@", log: log, type: .error, self.loggerVisualIndicator, self.loggerErrorVisualIndicator, error.localizedDescription)
+        }
     }
 }
